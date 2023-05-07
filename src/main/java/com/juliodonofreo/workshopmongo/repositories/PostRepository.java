6ae1e47f6e9688b68c7b02ccf9b0d91c68af5e4c
Julio Donofreo <julio.bibico@hotmail.com>
2023-05-07 14:54:18 -0300

    consulta simples por texto com query de string

--- src/main/java/com/juliodonofreo/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/juliodonofreo/workshopmongo/repositories/PostRepository.java
@@ -1,8 +1,13 @@
 package com.juliodonofreo.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.juliodonofreo.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
+
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
