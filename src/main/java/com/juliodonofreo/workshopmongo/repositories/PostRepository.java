f83f76b1a98f7a1700dcc83942176daeba2b6835
Julio Donofreo <julio.bibico@hotmail.com>
2023-05-08 16:53:48 -0300

    consulta alternativa com @query

--- src/main/java/com/juliodonofreo/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/juliodonofreo/workshopmongo/repositories/PostRepository.java
@@ -3,11 +3,14 @@ package com.juliodonofreo.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.juliodonofreo.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text); 
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
